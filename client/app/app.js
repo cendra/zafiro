74b4c67b9f479318658d3a81d8c643a28940e796
Agustin Moyano <agustin.moyano@gmail.com>
2015-02-10 12:53:28 -0300

    Se agregan directivas para buscar recursos en la aplicación actual, y se agrega un proveedor para manejar apis

--- client/app/app.js
+++ client/app/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('zafiro', [
+var zf = angular.module('zafiro', [
   'ngCookies',
   'ngResource',
   'ngSanitize',
@@ -9,18 +9,19 @@ angular.module('zafiro', [
   'ct.ui.router.extras',
   'ui.bootstrap',
   'oc.lazyLoad',
-  'toasty'
+  'toasty',
+  'angularSoap'
 ])
-  .config(function ($stateProvider, $urlRouterProvider, $locationProvider, $futureStateProvider) {
+  .config(function ($stateProvider, $urlRouterProvider, $locationProvider, $futureStateProvider, defaultUrl) {
     $urlRouterProvider
       .otherwise(function($injector, $location) {
         var host = $location.host().split('.')[0];
         var path = $location.path().split('/')[1];
 
-        if(host != path) {
+        if(host != path && path != defaultUrl) {
           return '/'+host+$location.url();
         } else {
-          return '/error/app';
+          return defaultUrl||'/error/app';
         }
 
       });
@@ -101,11 +102,131 @@ angular.module('zafiro', [
 
     $locationProvider.html5Mode(true);
   })
-  .run(function ($rootScope) {
-    $rootScope.$on('$stateNotFound', function(event, unfoundState, fromState, fromParams) {
-      console.log(event);
-      console.log(unfoundState);
-      console.log(fromState);
-      console.log(fromParams);
-    });
+  .constant('defaultUrl', '/yuli/login')
+  .provider('zafiro', function() {
+    var rest = {};
+    var wsdl = {};
+    var socket = {};
+    var self = this;
+
+    this.setRest = function(name, restBase) {
+      if(!name) return rest={};
+      if(!restBase&&angular.isObject(name)) {
+        return angular.forEach(name, function(value, key) { self.setRest(key, value); })
+      }
+      !restBase&&(restBase=name)&&(name='default');
+      rest[name] = restBase;
+    };
+
+    this.setSoap = function(name, wsdlUrl) {
+      if(!name) return wsdl={};
+      if(!wsdlUrl&&angular.isObject(name)) {
+        return angular.forEach(name, function(value, key) { self.setSoap(key, value); })
+      }
+      !wsdlUrl&&(wsdlUrl=name)&&(name='default');
+      wsdl[name] = wsdlUrl;
+    };
+
+    this.setSocket = function(name, sockBase) {
+      if(!name) return io={};
+      if(!sockBase&&angular.isObject(name)) {
+        return angular.forEach(name, function(value, key) { self.setSocket(key, value); }) 
+      }
+      !sockBase&&(sockBase=name)&&(name='default');
+      socket[name] = io.connect(sockBase);
+    };
+
+    this.$get = ['$http', '$soap',  function($http, $soap) {
+      return {
+        ptr: {
+          rest: 'default', 
+          soap: 'default',
+          socket: 'default'
+        },
+        set: function(name) {
+          if(!name||angular.isString(name)) {
+            angular.forEach(this.ptr, function(value, key, ptr) {
+              ptr[key] = name||'default'
+            });
+          } else {
+            this.ptr = angular.copy(name);
+          }
+          return this;
+        },
+        rest: function(options) {
+          if(options.url) {
+            var base = rest[this.ptr.rest]||(/^https?:\/\/.+/.test(this.ptr.rest)&&this.ptr.rest)||'';
+            options.url= base.replace(/\/+$/, '') + (base==''||/^\/.+/.test(options.url)?options.url:'/'+options.url);
+          } 
+          return $http(options);
+        },
+        get: function(url, options) {
+          options = options||{};
+          options.url = url;
+          options.method = 'GET';
+          return this.rest(options);
+        },
+        delete: function(url, options) {
+          options = options||{};
+          options.url = url;
+          options.method = 'DELETE';
+          return this.rest(options);
+        },
+        post: function(url, data, options) {
+          options = options||{};
+          options.url = url;
+          options.data = data;
+          options.method = 'POST';
+          return this.rest(options);
+        },
+        put: function(url, data, options) {
+          options = options||{};
+          options.url = url;
+          options.data = data;
+          options.method = 'PUT';
+          return this.rest(options);
+        },
+        on: function() {
+          if(socket[this.ptr.io]) {
+            return socket[this.ptr.io].on.apply(socket[this.ptr.io], Array.prototype.splice.call(arguments, 0))
+          }
+          throw 'Socket not available';
+        },
+        emit: function() {
+          if(socket[this.ptr.io]) {
+            return socket[this.ptr.io].emit.apply(socket[this.ptr.io], Array.prototype.splice.call(arguments, 0))
+          }
+          throw 'Socket not available';
+        },
+        call: function() {
+          var url = wsdl[this.ptr.soap]||(/^https?:\/\/.+/.test(this.ptr.soap)&&this.ptr.soap)||false;
+          if(url) {
+            var args = Array.prototype.splice.call(arguments, 0);
+            args.unshift(url);
+            return $soap.get.apply($soap, args);
+          }
+          throw 'Soap not available';
+        }
+      }
+    }];
+
   });
+
+['src', 'href'].forEach(function(refAttr) {
+  var refAttr = refAttr.toLowerCase();
+  var dirAttr = 'zf'+(refAttr[0].toUpperCase()+refAttr.substr(1));
+  zf.directive('zf'+(refAttr[0].toUpperCase()+refAttr.substr(1)), ['$location', function($location) {
+    return {
+      restrict: 'A',
+      link: function(scope, element, attrs) {
+        attrs.$observe(dirAttr, function(value) {
+          var project = $location.path().match(/^\/?([^\/]+)/);
+          if(project[1] && !new RegExp('^/app/'+project[1]+'/').test(value)) {
+            value = '/app/'+project[1]+'/'+value.replace(/^\/+/, '');
+          }
+          attrs.$set(refAttr, value);
+        });
+      }
+    }
+  }]);  
+});
