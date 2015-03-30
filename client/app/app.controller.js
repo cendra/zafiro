3acd641d3b42e205564f7a6a514995a1175b02d0
Agustin Moyano <agustin.moyano@gmail.com>
2015-03-30 10:44:31 -0300

    Done integration angular - polymer

--- client/app/app.controller.js
+++ client/app/app.controller.js
@@ -4,6 +4,7 @@ angular.module('zafiro')
 .controller('zafiroCtrl', function($scope, $state, $mdSidenav, $location) {
     
     $scope.polymer = "anda la osa!";
+    $scope.searchConfig = true;
     $scope.echo = function() {
       alert($scope.polymer);
     };
